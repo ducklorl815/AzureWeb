dbbb303c7895b78e4c0b7eb57d86ba842ed178b0
家毅 廖 <ducklorl815@gmail.com>
2022-10-12 17:06:44 +0800

    第一次上傳  人臉情緒辨識

---
+++ FaceApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace FaceApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtUrl.Text))
+            {
+                MessageBox.Show("OK~");
+            }
+            else
+            {
+                HttpClient client = new HttpClient();
+                using (HttpResponseMessage Response = await client.GetAsync(txtUrl.Text))
+                {
+                    Response.EnsureSuccessStatusCode();
+                    using (Stream imgStream = await Response.Content.ReadAsStreamAsync())
+                    {
+                        pictureBox1.Image = Image.FromStream(imgStream);
+                    }
+                    client = new HttpClient();
+                    var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+                    // Request headers
+                    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "3176efd4c2014aa7903981e7d0c143b6");
+
+                    // Request parameters
+                    queryString["returnFaceId"] = "true";
+                    queryString["returnFaceLandmarks"] = "false"; //五官位置
+                    queryString["returnFaceAttributes"] = "age, gender, emotion"; //偵測條件
+                    queryString["recognitionModel"] = "recognition_04"; //辨識模型
+                    queryString["returnRecognitionModel"] = "false";
+                    queryString["detectionModel"] = "detection_01"; //偵測模型
+                    queryString["faceIdTimeToLive"] = "86400"; //86400>>一天
+                    var uri = "https://msit143000face.cognitiveservices.azure.com/face/v1.0/detect?" + queryString;
+
+                    JObject data = new JObject { ["url"] = txtUrl.Text };
+                    string json = JsonConvert.SerializeObject(data);
+                    StringContent stringContent = new StringContent(json, Encoding.UTF8,"application/json");
+                    HttpResponseMessage jsonResponse = await client.PostAsync(uri, stringContent);
+                    jsonResponse.EnsureSuccessStatusCode();
+                    string result = await jsonResponse.Content.ReadAsStringAsync();
+                    //MessageBox.Show(result);
+                    dynamic faces = JsonConvert.DeserializeObject(result);
+                    foreach (var item in faces)
+                    {
+                        JObject face = item as JObject; 
+                        int age = Convert.ToInt32(face["faceAttributes"]["age"]); 
+                        string gender = Convert.ToString(face["faceAttributes"]["gender"]);
+                        JObject emotion = JObject.Parse(Convert.ToString(face["faceAttributes"]["emotion"]));
+
+                        float MaxEmotion = 0;
+                        string MaxEmotionName = "";
+
+                        foreach (JProperty prop in emotion.Properties())
+                        {
+                            float PropertyValue = float.Parse(Convert.ToString(emotion[prop.Name]));
+                            if (PropertyValue > MaxEmotion)
+                            {
+                                MaxEmotion=PropertyValue;
+                                MaxEmotionName = prop.Name;
+                            }
+                        }
+                        MessageBox.Show($"性別:{gender}\n年齡:{age}\n情緒:{MaxEmotionName}\n信心指數:{MaxEmotion * 100:n2}%");
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ VisionApp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace VisionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 9);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(695, 22);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.Text = "http://n.sinaimg.cn/sinacn20200109ac/237/w640h397/20200109/2c79-imvsvza1262467.jp" +
+    "g";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
